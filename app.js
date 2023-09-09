04b8d54e046b2fd43e34be76d7ead0298403bf77
YanManenti <y.a.martins@outlook.com>
2023-09-08 21:31:07 -0300

    CSS files, nodemon, gitignore, html names, simples express.

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-import { Express } from "express";
-var app = Express();
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
