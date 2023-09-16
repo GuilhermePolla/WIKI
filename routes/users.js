02f472fbb1fe46ab59b686e83b7f472f77142f10
GuilhermePolla <guilhermeborgespolla@hotmail.com>
2023-09-16 18:24:53 -0300

    Codigo de criação do JSON de usuarios e remoção do

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const fs = require('fs');
+const path = require('path');
 
 const router = express.Router();
 
@@ -8,8 +10,42 @@ router.get("/users_create", function (req, res, next) {
 
 router.post("/users_create", function (req, res, next) {
   const { nome, senha, repetirSenha, email } = req.body;
-  console.log("Dados Recebidos", { nome, senha, repetirSenha, email});
+  console.log("Dados Recebidos", { nome, senha, repetirSenha, email });
+  
+  if (senha === repetirSenha) {
+    console.log("As senha foi repetida corretamente");
+    
+    const caminhoArquivo = './data/users.json';
+
+    fs.readFile(caminhoArquivo, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Erro ao ler o arquivo JSON.');
+        return;
+      }
+
+      let conteudoJSON = {};
+      if (data) {
+        conteudoJSON = JSON.parse(data);
+      }
+
+      conteudoJSON.usuarios = conteudoJSON.usuarios || [];
+      conteudoJSON.usuarios.push({ nome, senha, email });
+
+      const jsonAtualizado = JSON.stringify(conteudoJSON, null, 2);
+
+      fs.writeFile(caminhoArquivo, jsonAtualizado, 'utf8', (err) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Erro ao escrever no arquivo JSON.');
+          return;
+        }
+        res.send('Dados adicionados com sucesso ao arquivo JSON.');
+      });
+    });
+  }
 });
+
 
 
 router.get("/users_edit/:id", function (req, res, next) {
