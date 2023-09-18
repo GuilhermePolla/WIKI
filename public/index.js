c6d8cb05962460188b45046009b2f58521f23ab3
YanManenti <y.a.martins@outlook.com>
2023-09-18 20:34:14 -0300

    Added like.

--- public/index.js
+++ public/index.js
@@ -1,14 +1,28 @@
-function getName() {
-  const name = localStorage.getItem("@loginWebII");
-  console.log(name);
-  return name;
-}
-getName();
-
 async function getArticles() {
   const articles = fetch("http://localhost:3000/destaques")
     .then((response) => {
-      if (response.status === 200) {
+      if (response.status === 201) {
+        return response.json();
+      }
+      return null;
+    })
+    .then((data) => {
+      // console.log(data);
+      return data;
+    });
+
+  return articles;
+}
+
+async function likeArticle(id) {
+  const like = fetch("http://localhost:3000/like/" + id, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => {
+      if (response.status === 201) {
         return response.json();
       }
       return null;
@@ -17,8 +31,22 @@ async function getArticles() {
       console.log(data);
       return data;
     });
+  return like;
+}
 
-  return articles;
+async function handleLike(id) {
+  const like = await likeArticle(id);
+  if (like === null) {
+    alert("Erro ao curtir artigo.");
+    return;
+  }
+  if (like.status === 201) {
+    alert("Artigo curtido com sucesso.");
+    renderArticles();
+  }
+  if (like.status === 400) {
+    alert("Erro ao curtir artigo.");
+  }
 }
 
 async function renderArticles() {
@@ -38,9 +66,13 @@ async function renderArticles() {
         <p class="card-text">${article.kb_body}</p>
         <p class="card-text">${article.kb_author_email}</p>
         <p class="card-text">${article.kb_liked_count}</p>  
+        <a id="like${article.kb_id}" class="btn btn-primary">Curtir</a>
         <a href="/articles/?id=${article.kb_id}" class="btn btn-primary">Ler</a>
       </div>
     `;
+    articleCard.querySelector("#like" + article.kb_id).onclick = () => {
+      handleLike(article.kb_id);
+    };
     articlesContainer.appendChild(articleCard);
   });
 }
