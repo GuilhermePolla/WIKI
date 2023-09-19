dd675eefab7788110c0941ddc415865ce4236ff7
YanManenti <y.a.martins@outlook.com>
2023-09-19 16:34:50 -0300

    Most of search by tags done.

--- public/index.js
+++ public/index.js
@@ -80,4 +80,54 @@ async function handleLike(id) {
   return articles;
 }
 
+async function getArticlesBySearch() {
+  const search = document.querySelector("#search").value;
+  if (search === "") {
+    const all = await getArticles();
+    return all;
+  }
+  const articles = fetch("http://localhost:3000/search/" + search)
+    .then((response) => {
+      if (response.status === 201) {
+        return response.json();
+      }
+      return null;
+    })
+    .then((data) => {
+      console.log(data);
+      return data;
+    });
+
+  return articles;
+}
+
+async function renderArticlesBySearch() {
+  const articles = await getArticlesBySearch();
+  const articlesContainer = document.querySelector("#articles");
+  articlesContainer.innerHTML = "";
+  if (articles === null) {
+    articlesContainer.innerHTML = "Erro ao carregar artigos";
+    return;
+  }
+  articles.forEach((article) => {
+    const articleCard = document.createElement("div");
+
+    articleCard.classList.add("card");
+    articleCard.innerHTML = `
+      <div class="card-body">
+        <h5 class="card-title">${article.kb_title}</h5>
+        <p class="card-text">${article.kb_body}</p>
+        <p class="card-text">${article.kb_author_email}</p>
+        <p class="card-text">${article.kb_liked_count}</p>
+        <a id="like${article.kb_id}" class="btn btn-primary">Curtir</a>
+        <a href="/articles/?id=${article.kb_id}" class="btn btn-primary">Ler</a>
+      </div>
+    `;
+    articleCard.querySelector("#like" + article.kb_id).onclick = () => {
+      handleLike(article.kb_id);
+    };
+    articlesContainer.appendChild(articleCard);
+  });
+}
+
 renderArticles();
